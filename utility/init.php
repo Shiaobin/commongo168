16692a5ea4557312fd35525905eb9a93f7f6aa5b
Shiaobin <Shiaobin@gmail.com>
2017-01-03 16:55:54 +0800

    在非根目錄也能正確判斷 DOCUMENT_ROOT

--- utility/init.php
+++ utility/init.php
@@ -1,7 +1,31 @@
 <?php
-	require_once("config/config.php");
-	require_once("utility/db.php");
-	require_once("utility/generator.php");
+	function getLongestMatchingSubstring($str1, $str2)
+	{
+	    $len_1 = strlen($str1);
+	    $longest = '';
+	    for($i = 0; $i < $len_1; $i++){
+	        for($j = $len_1 - $i; $j > 0; $j--){
+	            $sub = substr($str1, $i, $j);
+	            if (strpos($str2, $sub) !== false && strlen($sub) > strlen($longest)){
+	                $longest = $sub;
+	                break;
+	            }
+	        }
+	    }
+	    return $longest;
+	}
+
+	if(strpos($_SERVER['SCRIPT_FILENAME'],$_SERVER['DOCUMENT_ROOT'])===false){
+	    // how it works on reseller accounts...
+	    $docroot = getLongestMatchingSubstring(getcwd(), __FILE__).'/';
+	}else{
+	    // how it normally works...
+	    $docroot=$_SERVER['DOCUMENT_ROOT'].'/';
+	}
+
+	require_once($docroot."config/config.php");
+	require_once($docroot."utility/db.php");
+	require_once($docroot."utility/generator.php");
 	date_default_timezone_set('Asia/Taipei');
 	function now()
 	{
